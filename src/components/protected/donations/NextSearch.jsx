e02b30e3a708a669ad17e400e5df48a7350674b8
chinedu.ijeomah <chinedu.ijeomah@yahoo.com>
2023-04-07 22:52:17 +0100

    more new donations UI improvement

--- src/components/protected/donations/NextSearch.jsx
+++ src/components/protected/donations/NextSearch.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NextSearch = ({ setSearch, val, btnText }) => {
   return (
     <button 
-        className='w-[100px] p-1.5 border border-slate-600 bg-transparent text-slate-600 dark:bg-slate-600 dark:text-white dark:hover:bg-transparent dark:hover:text-slate-600 rounded-full hover:bg-slate-600 hover:text-white ml-64 mr-4'
+        className='w-[80px] p-1.5 border border-slate-600 bg-transparent text-slate-600 dark:bg-slate-600 dark:text-white dark:hover:bg-transparent dark:hover:text-slate-600 rounded-full hover:bg-slate-600 hover:text-white ml-64 mr-2 md:mr-4'
         onClick={(e) => setSearch(val)}
     >
         {btnText}
